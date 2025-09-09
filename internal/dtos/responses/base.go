f11b172c0b9de13d0627dcab110dcc0abe122167
Aldino Pratama <aldinopratama15@gmail.com>
2025-09-09 21:01:24 +0700

    create response base

---
+++ internal/dtos/responses/base.go
@@ -0,0 +1,16 @@
+package responses
+
+type BaseResponse[T any] struct {
+	Code    int           `json:"code,omitempty"`
+	Message string        `json:"message"`
+	Data    *T            `json:"data,omitempty"`
+	Paging  *PageMetadata `json:"paging,omitempty"`
+	Errors  error         `json:"errors,omitempty"`
+}
+
+type PageMetadata struct {
+	Page      int `json:"page"`
+	Size      int `json:"size"`
+	TotalItem int `json:"total_item"`
+	TotalPage int `json:"total_page"`
+}
