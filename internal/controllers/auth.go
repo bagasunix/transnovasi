53bf5975f36e26081f3a06832b7c909f48d1380a
Aldino Pratama <aldinopratama15@gmail.com>
2025-09-09 21:03:49 +0700

    ceate controller auth

---
+++ internal/controllers/auth.go
@@ -0,0 +1,63 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/phuslu/log"
+
+	"github.com/bagasunix/transnovasi/internal/dtos/requests"
+	"github.com/bagasunix/transnovasi/internal/dtos/responses"
+	"github.com/bagasunix/transnovasi/internal/repositories"
+	"github.com/bagasunix/transnovasi/internal/usecases"
+)
+
+type AuthController struct {
+	usecase usecases.AuthUsecase
+	logger  *log.Logger
+	repo    repositories.Repositories
+}
+
+func NewAuthController(logger *log.Logger, repo repositories.Repositories, usecase usecases.AuthUsecase) *AuthController {
+	return &AuthController{
+		usecase: usecase,
+		logger:  logger,
+		repo:    repo,
+	}
+}
+
+func (ac *AuthController) LoginUser(ctx *fiber.Ctx) error {
+	req := new(requests.Login)
+	result := new(responses.BaseResponse[*responses.ResponseLogin])
+
+	if err := ctx.BodyParser(req); err != nil {
+		result.Code = fiber.StatusBadRequest
+		result.Message = "Invalid request"
+		result.Errors = err
+		return ctx.Status(fiber.StatusBadRequest).JSON(result)
+	}
+
+	result = ac.usecase.LoginUser(ctx.Context(), req)
+	if result.Errors != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(result)
+	}
+
+	return ctx.Status(result.Code).JSON(result)
+}
+
+func (ac *AuthController) LoginCustomer(ctx *fiber.Ctx) error {
+	req := new(requests.Login)
+	result := new(responses.BaseResponse[*responses.ResponseLogin])
+
+	if err := ctx.BodyParser(req); err != nil {
+		result.Code = fiber.StatusBadRequest
+		result.Message = "Invalid request"
+		result.Errors = err
+		return ctx.Status(fiber.StatusBadRequest).JSON(result)
+	}
+
+	result = ac.usecase.LoginCustomer(ctx.Context(), req)
+	if result.Errors != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(result)
+	}
+
+	return ctx.Status(result.Code).JSON(result)
+}
