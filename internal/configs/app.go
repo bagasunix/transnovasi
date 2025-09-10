3c472111f04d08d950f0d0a0c0268405c42641ea
Aldino Pratama <aldinopratama15@gmail.com>
2025-09-10 11:26:01 +0700

    add logging in init app

--- internal/configs/app.go
+++ internal/configs/app.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/bagasunix/transnovasi/internal/controllers"
 	"github.com/bagasunix/transnovasi/internal/delivery/http"
+	"github.com/bagasunix/transnovasi/internal/middlewares"
 	"github.com/bagasunix/transnovasi/internal/repositories"
 	"github.com/bagasunix/transnovasi/internal/usecases"
 	"github.com/bagasunix/transnovasi/pkg/env"
@@ -25,7 +26,8 @@ func SetupApp(app *setupApp) *http.RouteConfig {
 	app.Log.Info().Msg("Setting up application...")
 	// setup repositories
 	repositories := repositories.New(app.Log, app.DB)
-
+	// pasang logging middleware di sini
+	app.App.Use(middlewares.LoggingMiddleware(app.Log, repositories))
 	// setup use cases
 	authUsecase := usecases.NewAuthUsecase(app.Log, app.DB, app.Cfg, repositories, app.Redis)
 	// setup controller
